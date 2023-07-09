e978ee1a4ce6eca77850b10214cb2ee5fe9b0a6c
vedanti <vedantiekre@gmail.com>
2023-07-09 23:51:26 +0530

    first version

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const cors = require('cors')
+const { request } = require('http')
+
+const app = express()
+app.use(cors('*'))
+
+app.get('/',(request, response) => {
+    response.send('this is version2 of my backend application')
+})
+
+app.listen(4000, '0.0.0.0', () => {
+    console.log('server started on port 4000')
+})
